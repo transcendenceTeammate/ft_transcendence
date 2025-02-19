a54d553218d4f0a4934ac06a1a5e4ec6d89165de
olimarmite <37936816+olimarmite@users.noreply.github.com>
2025-02-19 19:38:00 +0100

    :wrench: Update docker config

--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -13,18 +13,6 @@ if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
       -subj "/C=AT/ST=W/L=W/O=42/OU=42k/CN=localhost"
 fi
 
-# Check if the user_management service on port 8000 is up.
-while true; do
-    if nc -z -w 2 user_management 8000; then
-        echo "User Management service is up!"
-        sleep 1
-        break
-    else
-        echo "User Management service isn't up...waiting..."
-        sleep 4
-    fi
-done
-
 echo "Ready! Open: https://localhost:8443/"
 nginx -g "daemon off;"
 #nginx
