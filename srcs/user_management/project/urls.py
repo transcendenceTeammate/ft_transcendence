2812b481e81d497000eb3266b7facfad48e56bfd
jacobosss <taiebjacob55@gmail.com>
2025-03-17 11:39:52 +0100

    configured Docker setup for media handling; added media volume and health checks; updated URL patterns for serving media in debug mode

--- srcs/user_management/project/urls.py
+++ srcs/user_management/project/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
 
 	path('', views.index),
 	re_path(r'^.*$', views.index),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
