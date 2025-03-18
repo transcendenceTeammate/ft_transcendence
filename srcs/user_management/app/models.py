8f61df25800394ba77bfd9e33a88bf369f198fb0
jacobosss <taiebjacob55@gmail.com>
2025-03-18 19:48:46 +0100

    refactor image handling in user profile; disable Nginx autoindex for media and optimize image upload process

--- srcs/user_management/app/models.py
+++ srcs/user_management/app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+import uuid
+import os
 from django.contrib.auth.models import User
+from PIL import Image
 
 class UserType(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_type')
@@ -12,19 +15,25 @@ class UserType(models.Model):
 	def __str__(self):
 		return f"{self.user.username} - {self.get_user_type_display()}"
 
-
-# class UserProfile(models.Model):
-# 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='image_file')
-# 	UserType = models.OneToOneField(UserType, on_delete=models.CASCADE, related_name='user_type')
-# 	image = models.OneToOneField(Images, on_delete=model, related_name
-
-# 	def __str__(self):
-# 		return f"{self.user.username} - {self.image_data}"
-
+def upload_to(instance, filename):
+	ext = filename.split('.')[-1]
+	unique_filename = f"{uuid.uuid4()}.{ext}"
+	return os.path.join('profile_pics/', unique_filename)
 
 class ImageFile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='image_file')
-	image = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to=upload_to, null=True, blank=True)  
+
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
+
+		if self.image:
+			img_path = self.image.path
+			img = Image.open(img_path)  
+
+			max_size = (300, 300)
+			img.thumbnail(max_size, Image.LANCZOS)
+			img.save(img_path)
 
 	def __str__(self):
-		return f"{self.user.username} - {self.image_data}"
+		return f"{self.user.username} - {self.image}"
