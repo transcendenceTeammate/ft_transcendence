d24c01d4d2ef26b8c0b757c5997ee979fc5a6cff
svetlaan <nyagalen@gmail.com>
2025-03-24 18:20:21 +0100

    took info from deleted file into a comment

--- srcs/front/js/views/Navbar.js
+++ srcs/front/js/views/Navbar.js
@@ -50,3 +50,15 @@ export default class Navbar extends AbstractView {
         `
     }
 }
+
+// <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                // 	<li class="nav-item">
+                // 		<a class="nav-link active" aria-current="page" href="#">My profile</a>
+                // 	</li>
+                // 	<li class="nav-item">
+                // 		<a class="nav-link" href="#">All players</a>
+                // 	</li>
+                // 	<li class="nav-item">
+                // 		<a class="nav-link" href="#">Smth else</a>
+                // 	</li>
+                // </ul>
