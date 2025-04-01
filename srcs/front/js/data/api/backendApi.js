ebddc1ba3f7fc0631a4ecb37aeeb78d0753b86ae
olimarmite <37936816+olimarmite@users.noreply.github.com>
2025-04-02 00:52:34 +0200

    feat: implement HttpClient with middleware support for authentication and error handling (#47)

--- srcs/front/js/data/api/backendApi.js
+++ srcs/front/js/data/api/backendApi.js
@@ -1,114 +1,116 @@
 import { getCookie } from "../../utils/getCookie.js";
 import CONFIG from "../../config.js"
+import { HttpClient, Middleware } from "../../core/HttpClient.js";
 
-class HttpClient {
-	constructor(baseUrl) {
-		this.baseUrl = baseUrl;
-	}
 
-	_buildOptions(method, data = null, headers = {}, isJson = true) {
-		const options = {
-			method,
-			headers: {
-				...(isJson ? { 'Content-Type': 'application/json' } : {}),
-				...headers,
-			},
-			credentials: 'include',
-		};
+class AuthMiddleware extends Middleware {
 
-		if (data && isJson) {
-			options.body = JSON.stringify(data);
-		} else if (data && !isJson) {
-			options.body = data;
-		}
-
-		return options;
-	}
-
-	async _fetch(endpoint, options) {
-		const response = await fetch(`${this.baseUrl}/${endpoint}`, options);
-		if (!response.ok) {
-			const errorData = await response.json();
-			throw new Error(errorData.error || 'Request failed');
-		}
-		return response.json();
-	}
-
-	async get(endpoint, headers = {}) {
-		const options = this._buildOptions('GET', null, headers);
-		return this._fetch(endpoint, options);
-	}
-
-	async post(endpoint, data, headers = {}, isJson = true) {
-		const options = this._buildOptions('POST', data, headers, isJson);
-		return this._fetch(endpoint, options);
-	}
-
-	async patch(endpoint, data, headers = {}) {
-		const options = this._buildOptions('PATCH', data, headers);
-		return this._fetch(endpoint, options);
-	}
-
-	async delete(endpoint, data, headers = {}) {
-		const options = this._buildOptions('DELETE', data, headers);
-		return this._fetch(endpoint, options);
-	}
-}
-
-export class BackendApi {
-	constructor() {
-		this._baseUrl = `${CONFIG.API_URL}`;
-		this._httpClient = new HttpClient(this._baseUrl);
-	}
-
-	// ==== Helpers ====
 	_getAuthHeaders() {
 		const accessToken = getCookie('access_token');
+		if (accessToken == null) {
+			return null;
+		}
 		return {
 			Authorization: `Bearer ${accessToken}`,
 		};
 	}
 
+	_onUnauthenticated() {
+		console.error("User is unauthenticated...");
+		window.location.href = "/";
+	}
+
+	async interceptRequest(endpoint, options, next = async () => { }) {
+		const authHeaders = this._getAuthHeaders();
+		if (authHeaders != null) {
+			options.headers = { ...options.headers, ...authHeaders };
+		}
+		const response = await next(endpoint, options);
+
+		if (response.status === 401) {
+			this._onUnauthenticated();
+			throw new Error("Unauthenticated");
+		}
+
+		return response;
+	}
+}
+
+class ErrorHandelingMiddleware extends Middleware {
+
+	async interceptRequest(endpoint, options, next = async () => { }) {
+
+		const response = await next(endpoint, options);
+
+		if (!response.ok) {
+			const errorData = await response.json();
+			throw new Error(errorData.error || 'Request failed');
+		}
+
+		return response;
+	}
+}
+
+
+
+export class BackendApi {
+	constructor() {
+		this._baseUrl = `${CONFIG.API_URL}`;
+		this._httpClient = new HttpClient(this._baseUrl);
+		this._httpClient.registerMiddleware(AuthMiddleware);
+		this._httpClient.registerMiddleware(ErrorHandelingMiddleware);
+
+		
+	}
+
 	// ==== User ====
-	getUserData() {
-		return this._httpClient.get("api/users/me/", this._getAuthHeaders());
+	async getUserData() {
+		const response = await this._httpClient.get("api/users/me/", {});
+		return response.json()
 	}
 
-	setUsername(newUsername) {
-		return this._httpClient.patch("api/users/update-username/", { username: newUsername }, this._getAuthHeaders());
+	async setUsername(newUsername) {
+		const response = await this._httpClient.patch("api/users/update-username/", { username: newUsername }, {});
+		return response.json()
 	}
 
-	uploadUserAvatar(image) {
+	async uploadUserAvatar(image) {
 		const formData = new FormData();
 		formData.append('image', image);
-		return this._httpClient.post("api/users/upload-profile-picture/", formData, this._getAuthHeaders(), false);
+		const response = await this._httpClient.post("api/users/upload-profile-picture/", formData, {}, false);
+		return response.json()
 	}
 
 	// ==== Friends ====
-	getFriendList() {
-		return this._httpClient.get("api/friend/list/", this._getAuthHeaders());
+	async getFriendList() {
+		const response = await this._httpClient.get("api/friend/list/", {});
+		return response.json()
 	}
 
-	addFriend(friendNickname) {
-		return this._httpClient.post("api/friend/add/", { friend_nickname: friendNickname }, this._getAuthHeaders());
+	async addFriend(friendNickname) {
+		const response = await this._httpClient.post("api/friend/add/", { friend_nickname: friendNickname }, {});
+		return response.json()
 	}
 
-	removeFriend(friendNickname) {
-		return this._httpClient.delete("api/friend/remove/", { friend_nickname: friendNickname }, this._getAuthHeaders());
+	async removeFriend(friendNickname) {
+		const response = await this._httpClient.delete("api/friend/remove/", { friend_nickname: friendNickname }, {});
+		return response.json()
 	}
 
 	// ==== Game ====
-	getUserGameHistory() {
-		return this._httpClient.get("api/game/list/", this._getAuthHeaders());
+	async getUserGameHistory() {
+		const response = await this._httpClient.get("api/game/list/", {});
+		return response.json()
 	}
 
-	createGame(player1Id, player2Id, score1, score2) {
+	async createGame(player1Id, player2Id, score1, score2) {
 		const payload = {
 			player_1: player1Id,
 			player_2: player2Id,
 			score_1: score1,
 			score_2: score2,
 		};
-		return this._httpClient.post("api/game/add/", payload, this._getAuthHeaders());
+		const response = await this._httpClient.post("api/game/add/", payload, {});
+		return response.json()
 	}
 }
