4365af891907cc82f77bbb1fa749fecff1607f4e
jacobosss <taiebjacob55@gmail.com>
2025-04-15 20:32:11 +0200

    refactor: remove dotenv usage from views and settings

--- srcs/user_management/project/settings.py
+++ srcs/user_management/project/settings.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
