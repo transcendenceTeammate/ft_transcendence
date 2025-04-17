2c9f375916e801b086783716f2cfffa3d4729ee4
Noe SPECIEL <nspeciel@paul-f4Ar6s2.clusters.42paris.fr>
2025-04-17 18:37:07 +0200

    add game stat

--- srcs/user_management/app/views.py
+++ srcs/user_management/app/views.py
@@ -60,7 +60,7 @@ def login(request):
 	response = Response({
 		"user": serializer.data
 	})
-	response.set_cookie('access_token', str(access_token), httponly=False, secure=True, samesite='Lax', domain='.app.10.24.5.3.nip.io')
+	response.set_cookie('access_token', str(access_token), httponly=False, secure=True, samesite='Lax', domain='.app.10.24.6.2.nip.io')
 	return response
 
 @api_view(['POST'])
@@ -79,7 +79,7 @@ def signup(request):
 			"type": user.profile.type,
 			"picture": user.profile.picture.url if user.profile.picture else None
 		})
-		response.set_cookie('access_token', str(access_token), httponly=False, secure=True, samesite='Lax', domain='.app.10.24.5.3.nip.io')
+		response.set_cookie('access_token', str(access_token), httponly=False, secure=True, samesite='Lax', domain='.app.10.24.6.2.nip.io')
 		return response
 
 	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -142,7 +142,7 @@ def auth42(request):
 	access_token = AccessToken.for_user(user)
 
 	response = HttpResponseRedirect(os.getenv('BASE_URL') + '/start-game')
-	response.set_cookie('access_token', str(access_token), httponly=False, secure=True, samesite='Lax', domain='.app.10.24.5.3.nip.io')
+	response.set_cookie('access_token', str(access_token), httponly=False, secure=True, samesite='Lax', domain='.app.10.24.6.2.nip.io')
 
 	return response
 
@@ -152,7 +152,7 @@ def get_access_token(request):
 	if request.method == "POST":
 		client_id = "VOTRE_CLIENT_ID"
 		client_secret = "VOTRE_CLIENT_SECRET"
-		redirect_uri = "http://10.24.5.3.nip.io:8443/auth42/"
+		redirect_uri = "http://10.24.6.2.nip.io:8443/auth42/"
 		code = request.POST.get("code")
 
 		url = "https://api.intra.42.fr/oauth/token"
