5fa20eea0f282ce9d24399372ab83b5f33c342da
Henri BERNARD <hebernar@paul-f4Ar1s3.clusters.42paris.fr>
2025-04-22 12:17:44 +0200

    Modif entrypoint openssl command expand ip

--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -47,6 +47,7 @@ fi
 
 CERT_PATH="/etc/nginx/ssl/localhost.crt"
 KEY_PATH="/etc/nginx/ssl/localhost.key"
+CN_PATH="${BASE_DOMAIN%.nip.io}"
 
 if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
     echo "Generating SSL certificate..."
@@ -54,7 +55,7 @@ if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
       -keyout "$KEY_PATH" \
       -out "$CERT_PATH" \
-      -subj "/C=AT/ST=W/L=W/O=42/OU=42k/CN=10.24.5.3"
+      -subj "/C=AT/ST=W/L=W/O=42/OU=42k/CN=$CN_PATH"
 fi
 
 exec "$@"
